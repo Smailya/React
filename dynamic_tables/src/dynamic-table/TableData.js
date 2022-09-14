ab612334c1f7aaf4ac7c819691f3878a560e971c
Ismail Cisse <ismaelcisse015@gmail.com>
2022-09-14 15:08:24 +0200

    first-app

---
+++ dynamic_tables/src/dynamic-table/TableData.js
@@ -0,0 +1,12 @@
+//TableData.js
+
+const TableData=[
+    {id:1, fullName:"Noor Khan", age:25, city:"Patna", Email: "noorkhan@5gmail.com"},
+    {id:2, fullName:"Rapsan Jani", age:26, city:"Noida" , Email: "noorkhan@5gmail.com"},
+    {id:3, fullName:"Monika Singh", age:18, city:"New Delhi", Email: "noorkhan@5gmail.com"},
+    {id:4, fullName:"Sunil Kumar", age:22, city: "Jaipur", Email: "noorkhan@5gmail.com"},
+    {id:5, fullName:"Kajol Kumari", age: 21, city: "Chennai", Email: "noorkhan@5gmail.com"},
+    {id:6, fullName:"Isamil Cisse", age: 21, city: "Johanesburg", Email: "noorkhan@5gmail.com"}
+    
+]
+export default TableData;
